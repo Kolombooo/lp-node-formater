5dd8fb26a843d66ff6d0ca58f7deb26548a28ac8
Kolombo <h.kolombooo@gmail.com>
2023-01-22 16:56:42 +0100

    Beta

---
+++ lp-node-formater/FormatNode.cs
@@ -0,0 +1,10 @@
+﻿namespace lp_node_formater;
+
+public class FormatNode
+{
+    public string NodeFormater(string input)
+    {
+        string format = "/addperms h " + input + " true global";
+        return format;
+    }
+}
---
+++ lp-node-formater/Program.cs
@@ -0,0 +1,6 @@
+﻿using lp_node_formater;
+
+string Input = "cmi.command.whowas";
+string Result = new FormatNode().NodeFormater(Input);
+
+Console.WriteLine(Result);
