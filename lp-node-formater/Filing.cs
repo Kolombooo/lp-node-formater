bc0ca4061fcfffa731eb6c697459ab39faf2b37f
Kolombo <h.kolombooo@gmail.com>
2023-01-22 17:25:46 +0100

    Release 1.0

---
+++ lp-node-formater/Filing.cs
@@ -0,0 +1,12 @@
+﻿namespace lp_node_formater;
+
+using System.IO;
+using System.Text;
+
+public class Filing
+{
+    public string ContentExtractor(string FilePath)
+    {
+        return File.ReadAllText(FilePath);
+    }
+}
--- lp-node-formater/Program.cs
+++ lp-node-formater/Program.cs
@@ -1,6 +1,14 @@
 ﻿using lp_node_formater;
 
-string Input = "cmi.command.whowas";
-string Result = new FormatNode().NodeFormater(Input);
+string FileContent = new Filing().ContentExtractor(@"c:\Users\Kolombo\RiderProjects\lp-node-formater\lp-node-formater\resources\input.txt");
+List<string> FileContentList = new List<string>(
+    FileContent.Split(new string[] { "\r\n" }, 
+        StringSplitOptions.RemoveEmptyEntries));
 
-Console.WriteLine(Result);
+
+foreach (var i in FileContentList)
+{
+    var nodeFormater = i;
+    nodeFormater = new FormatNode().NodeFormater(i);
+    Console.WriteLine(nodeFormater);
+}
